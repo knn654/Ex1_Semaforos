53ce6f0c72e11e0f61f6053322c5eb73b1116d94
knn654 <kaua.nascimento@fatec.sp.gov.br>
2023-03-23 15:19:11 -0300

    .

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,6 @@ import java.util.concurrent.Semaphore;
 
 public class Main {
 public static Semaphore semaforo;
-	/* 
-Um grande show acontecerá no Brasil, em uma casa com capacidade para 100 pessoas.
-A venda será feita excluisvamente pelo sistema.
-Simultaneamente, 300 pessoas, no primeiro instante acessam o sistema de compra.
-*/
 	public static void main(String[] args) {
 		
 		int totalCompradores = 300;
